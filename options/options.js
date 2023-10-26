a4f72889d751384bb24e839ff33b407e9eb88a43
Crowdbotics <team@crowdbotics.com>
2023-10-26 19:44:02 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "meca-finder1-44168",
+  url: "https://meca-finder1-44168.botics.co",
+  api: "https://meca-finder1-44168.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
